2ebf0a66d6a30d44d43c18fc7658a394ee452b37
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-12 22:16:26 +0900

    Time: 2036 ms (74.13%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-SELECT CASE WHEN eu.unique_id is NULL THEN NULL
-            ELSE eu.unique_id
-        END unique_id,
-        e.name
+SELECT unique_id, e.name
 FROM Employees e
 LEFT JOIN EmployeeUNI eu on e.id = eu.id
