8d421c0ab89e1ade25ea02d8f5f1e1352e88fb07
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-14 21:25:36 +0900

    Time: 369 ms (84.77%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,16 @@
-# Write your MySQL query statement below
-SELECT sub.machine_id, round(AVG(sub.difference),3) processing_time
-FROM
-(SELECT a_e.machine_id, a_e.process_id , 
-    round(MAX(CASE WHEN activity_type = 'end' THEN timestamp END) -
-    MAX(CASE WHEN activity_type = 'start' THEN timestamp END),3) AS difference
-FROM Activity a_e
-GROUP BY a_e.machine_id, a_e.process_id) sub
-GROUP BY sub.machine_id
+WITH end_val AS(
+    SELECT a_e.machine_id, a_e.process_id , a_e.timestamp end_time
+    FROM Activity a_e
+    WHERE a_e.activity_type='end'
+    GROUP BY a_e.machine_id, a_e.process_id
+),
+    start_val AS(
+    SELECT a_s.machine_id, a_s.process_id , a_s.timestamp start_time
+    FROM Activity a_s
+    WHERE a_s.activity_type='start'
+    GROUP BY a_s.machine_id, a_s.process_id
+    )
+SELECT e.machine_id, 
+    ROUND(SUM(e.end_time - s.start_time)/count(e.process_id),3) processing_time
+FROM end_val e INNER JOIN start_val s ON e.machine_id = s.machine_id and e.process_id = s.process_id
+GROUP BY e.machine_id;
