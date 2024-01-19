eb4fcd883560862e992cc5fb10feb9fbc34a0931
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-19 16:27:47 +0900

    Time: 1184 ms (31.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+WITH first AS(
+    #연속적으로 로그인 한 경우 아이디
+    SELECT a_m.player_id,
+            MIN(a_m.event_date) first_date
+    FROM Activity a_m
+    GROUP BY a_m.player_id
+) 
+SELECT ROUND(COUNT(DISTINCT a.player_id)/
+            (SELECT COUNT(DISTINCT a_s.player_id) FROM Activity a_s)
+            ,2) fraction
+FROM Activity a
+INNER JOIN first f ON a.player_id = f.player_id
+AND DATE_ADD(f.first_date, INTERVAL 1 DAY) = a.event_date
