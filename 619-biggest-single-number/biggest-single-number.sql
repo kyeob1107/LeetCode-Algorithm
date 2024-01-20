498bdd7a8cf50278058c5efab54f066e38f92127
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-20 13:08:39 +0900

    Time: 675 ms (85.13%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -4,6 +4,13 @@ WITH single AS (
     FROM MyNumbers m_s
     GROUP BY m_s.num
     HAVING COUNT(m_s.num) = 1
+),
+max_n AS (
+    SELECT m_m.num
+    FROM MyNumbers m_m
+    GROUP BY m_m.num
+    HAVING MAX(m_m.num) = m_m.num
 )
 SELECT MAX(s.num) num
-FROM single s
+FROM max_n m
+LEFT JOIN single s ON m.num = s.num
