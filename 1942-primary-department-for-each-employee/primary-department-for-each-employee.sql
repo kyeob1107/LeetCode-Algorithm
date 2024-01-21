2a6e7e181020389ea4dd3e3ea887ceeee8514d87
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-21 14:00:09 +0900

    Time: 1203 ms (26.09%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
 SELECT e.employee_id, e.department_id
 FROM Employee e
-GROUP BY e.employee_id
-HAVING COUNT(e.department_id) = 1
-UNION
-SELECT e2.employee_id, e2.department_id FROM Employee e2 WHERE e2.primary_flag = 'Y'
+WHERE e.primary_flag = 'Y' OR e.employee_id in (
+    SELECT e2.employee_id
+    FROM Employee e2
+    GROUP BY e2.employee_id
+    HAVING COUNT(*) = 1
+)
