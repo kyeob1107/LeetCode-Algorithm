d180e32f971e98165f9c7a0167d17617aa756018
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-21 14:22:39 +0900

    Time: 1078 ms (44.49%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
-with cnt1 as (
-    select e2.employee_id, count(e2.department_id) cnt from Employee e2
-    group by e2.employee_id
-    )
-select e.employee_id, e.department_id
-from Employee e
-join cnt1 c1
-on e.employee_id = c1.employee_id
-where e.primary_flag = (case when c1.cnt = 1 then e.primary_flag else 'Y' end);
+SELECT e.employee_id, e.department_id
+FROM Employee e
+GROUP BY e.employee_id
+HAVING COUNT(e.department_id) = 1
+UNION
+SELECT e2.employee_id, e2.department_id FROM Employee e2 
+WHERE e2.primary_flag = 'Y'
