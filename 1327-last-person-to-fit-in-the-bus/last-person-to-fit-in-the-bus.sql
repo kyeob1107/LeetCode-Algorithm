af8f34e45fb8f02f0e815396bfe007d3f38155b2
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-22 10:23:11 +0900

    Time: 3138 ms (17.88%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
-WITH total_by_turn AS (
-    SELECT q.turn, q.person_id, q.person_name, q.weight,
-    (SELECT SUM(q_s.weight) FROM Queue q_s 
-                            WHERE q_s.turn<=q.turn) sum_weight
-    FROM Queue q
-    ORDER BY q.turn DESC
-)
-SELECT tbt.person_name
-FROM total_by_turn tbt
-WHERE tbt.sum_weight <=1000
+SELECT q.person_name
+FROM Queue q
+WHERE 1000 >= (
+                SELECT SUM(q_s.weight) 
+                FROM Queue q_s 
+                WHERE q_s.turn<=q.turn
+                )
+ORDER BY q.turn DESC
 LIMIT 1
