c95ea662f1956270dee4f69113e38c2f42f6451f
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-22 10:24:19 +0900

    Time: 2555 ms (35.51%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,9 +2,9 @@
 SELECT q.person_name
 FROM Queue q
 WHERE 1000 >= (
-                SELECT SUM(q_s.weight) 
-                FROM Queue q_s 
-                WHERE q_s.turn<=q.turn
-                )
+    SELECT SUM(q_s.weight) 
+    FROM Queue q_s 
+    WHERE q_s.turn<=q.turn
+)
 ORDER BY q.turn DESC
 LIMIT 1
