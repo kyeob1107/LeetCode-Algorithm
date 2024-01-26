2c87d0485a70b5271efe86ac13e656d22e17b892
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-26 12:27:09 +0900

    Time: 1102 ms (91.40%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,9 +1,17 @@
 # Write your MySQL query statement below
+-- WITH temp AS (
+--     SELECT p.email, MIN(p.id) AS min_id
+--     FROM Person p
+--     GROUP BY p.email
+--     HAVING COUNT(*) > 1)
+
+-- DELETE p
+-- FROM Person p
+-- JOIN temp t ON p.email = t.email AND p.id <> t.min_id
 WITH temp AS (
     SELECT p.email, MIN(p.id) AS min_id
     FROM Person p
-    GROUP BY p.email
-    HAVING COUNT(*) > 1)
+    GROUP BY p.email)
 
 DELETE p
 FROM Person p
